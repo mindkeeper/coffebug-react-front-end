168a21279ec1fdbf415729f6e4f1b944576ab763
mindkeeper <fcb.nyak@gmail.com>
2022-10-28 18:08:06 +0700

    add route

---
+++ src/helpers/router.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
+import Products from "../pages/Products/Products";
+import Profile from "../pages/Profile/Profile";
+import SignUp from "../pages/SignUp/SignUp";
+import DetailsProduct from "../pages/DetailsProduct/Detailsproduct";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <SignUp />,
+  },
+  {
+    path: "/products",
+    element: <Products />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+  },
+  {
+    path: "/details",
+    element: <DetailsProduct />,
+  },
+]);
+
+export default router;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Login from "./pages/Login/Login";
+import { RouterProvider } from "react-router-dom";
+import router from "./helpers/router";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Login />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
