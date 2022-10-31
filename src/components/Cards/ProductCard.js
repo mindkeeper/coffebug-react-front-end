d781fe22e39f9ca5c2c7086c02e8fd9feb628443
mindkeeper <fcb.nyak@gmail.com>
2022-10-31 23:37:25 +0700

    adding navigate to product detail and dynamic data

--- src/components/Cards/ProductCard.js
+++ src/components/Cards/ProductCard.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import styles from "./styles/ProductCard.module.css";
-import cardImg from "../../assets/img/products/veggie.png";
-const ProductCard = () => {
+import withNavigate from "../../helpers/withNavigate";
+
+const ProductCard = ({ navigate, id, productName, price, image }) => {
   return (
-    <div className={styles["product-card"]}>
-      <img src={cardImg} alt="" className={styles["product-img"]} />
-      <p className={styles["product-name"]}>Veggie tomato Mix</p>
-      <p className={styles["product-price"]}>IDR 34.000</p>
-    </div>
+    <Fragment>
+      <div
+        className={styles["product-card"]}
+        onClick={() => {
+          navigate(`/products/${id}`);
+        }}
+      >
+        <img
+          src={`http://localhost:8080/${image}`}
+          alt=""
+          className={styles["product-img"]}
+        />
+        <p className={styles["product-name"]}>{productName}</p>
+        <p className={styles["product-price"]}>{price}</p>
+      </div>
+    </Fragment>
   );
 };
 
-export default ProductCard;
+export default withNavigate(ProductCard);
