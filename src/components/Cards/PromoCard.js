cbdc62a5865f277bdaa7e054b868ef7f23920669
mindkeeper <fcb.nyak@gmail.com>
2022-10-31 23:37:57 +0700

    fixing styles

--- src/components/Cards/PromoCard.js
+++ src/components/Cards/PromoCard.js
@@ -4,14 +4,14 @@ import mothers from "../../assets/img/products/mothers.png";
 const PromoCard = () => {
   return (
     <div
-      class={`col-12 ${styles["promo-card"]} row align-items-center ${styles["green"]}`}
+      className={`col-12 ${styles["promo-card"]} row align-items-center ${styles["green"]}`}
     >
-      <div class={`col-4 ${styles["promo-img"]}`}>
+      <div className={`col-4 ${styles["promo-img"]}`}>
         <img src={mothers} alt="" />
       </div>
-      <div class={`col-8 ${styles["promo-desc-container"]}`}>
-        <p class={styles["promo-name"]}>HAPPY MOTHER’S DAY!</p>
-        <p class={styles["promo-desc"]}>
+      <div className={`col-8 ${styles["promo-desc-container"]}`}>
+        <p className={styles["promo-name"]}>HAPPY MOTHER’S DAY!</p>
+        <p className={styles["promo-desc"]}>
           Get one of our favorite menu for free!
         </p>
       </div>
